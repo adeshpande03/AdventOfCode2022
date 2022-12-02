a8c6d5b533ee129d4cc09004528fba0bddef8abb
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-02 17:44:56 -0600

    made day2 faster

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -3,7 +3,7 @@ def part1(fileName):
 
 
 def part2(fileName):
-    return sum([{"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) - 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) > 0 else ["X", "Y", "Z"][2])] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 0 else {"X": 1, "Y": 2, "Z": 3}[{"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 3 else {"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) + 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) < 2 else ["X", "Y", "Z"][0])] for line in open(fileName, "r")]) + sum([{"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] for line in open(fileName, "r")])
+    return sum([{"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) - 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) > 0 else ["X", "Y", "Z"][2])] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 0 else {"X": 1, "Y": 2, "Z": 3}[{"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 3 else {"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) + 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) < 2 else ["X", "Y", "Z"][0])] for line in open(fileName, "r")] + [{"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] for line in open(fileName, "r")])
     
 
 if __name__ == "__main__":

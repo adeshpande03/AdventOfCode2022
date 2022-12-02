57a3924463dab10c5a3550cb14c8477c775c3d1a
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-02 15:28:52 -0600

    bigs fixed!

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -3,40 +3,8 @@ def part1(fileName):
 
 
 def part2(filename):
-    f = open(filename, "r")
-    oppPossible = ["X", "Y", "Z"]
-    resultKey = {"X": 0, "Y": 3, "Z": 6}
-    choiceKey = {"X": 1, "Y": 2, "Z": 3}
-    matchingOppToMe = {"A": "X", "B": "Y", "C": "Z"}
-
-    def pickMe(opponent, result):
-        if result == 0:
-            myChoice = (
-                oppPossible[oppPossible.index(opponent) - 1]
-                if oppPossible.index(opponent) > 0
-                else oppPossible[2]
-            )
-        elif result == 3:
-            myChoice = opponent
-        elif result == 6:
-            myChoice = (
-                oppPossible[oppPossible.index(opponent) + 1]
-                if oppPossible.index(opponent) < 2
-                else oppPossible[0]
-            )
-        return choiceKey[myChoice]
-
-    return sum(
-        [
-            pickMe(
-                matchingOppToMe[line.strip().split()[0]],
-                resultKey[line.strip().split()[1]],
-            )
-            + resultKey[line.strip().split()[1]]
-            for line in f
-        ]
-    )
-
+    return sum([{"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) - 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) > 0 else ["X", "Y", "Z"][2])] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 0 else {"X": 1, "Y": 2, "Z": 3}[{"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 3 else {"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) + 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) < 2 else ["X", "Y", "Z"][0])] for line in open(filename, "r")]) + sum([{"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]]for line in open(filename, "r")])
+    
 
 if __name__ == "__main__":
     print(part1("input.txt"), part2("input.txt"))

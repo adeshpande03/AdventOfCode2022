f1eb58b9bcb47dd5e14c0e8471679a336cb5ca29
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-03 16:33:52 -0600

    Fixed spacing because it was annoying

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,11 +1,74 @@
 def part1(fileName):
-    return sum([{"X": 1, "Y": 2, "Z": 3}.get(line.strip().split()[1]) + (6 if ["X", "Y", "Z"].index(line.strip().split()[1]) - ["A", "B", "C"].index(line.strip().split()[0]) in [1, -2] else 3 if ["X", "Y", "Z"].index(line.strip().split()[1]) - ["A", "B", "C"].index(line.strip().split()[0]) == 0 else 0) for line in open(fileName, "r")])
+    return sum(
+        [
+            {"X": 1, "Y": 2, "Z": 3}.get(line.strip().split()[1])
+            + (
+                6
+                if ["X", "Y", "Z"].index(line.strip().split()[1])
+                - ["A", "B", "C"].index(line.strip().split()[0])
+                in [1, -2]
+                else 3
+                if ["X", "Y", "Z"].index(line.strip().split()[1])
+                - ["A", "B", "C"].index(line.strip().split()[0])
+                == 0
+                else 0
+            )
+            for line in open(fileName, "r")
+        ]
+    )
 
 
 def part2(fileName):
-    return sum([{"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) - 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) > 0 else ["X", "Y", "Z"][2])] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 0 else {"X": 1, "Y": 2, "Z": 3}[{"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]] if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 3 else {"X": 1, "Y": 2, "Z": 3}[(["X", "Y", "Z"][["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) + 1] if ["X", "Y", "Z"].index({"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]) < 2 else ["X", "Y", "Z"][0])] for line in open(fileName, "r")] + [{"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] for line in open(fileName, "r")])
-    
+    return sum(
+        [
+            {"X": 1, "Y": 2, "Z": 3}[
+                (
+                    ["X", "Y", "Z"][
+                        ["X", "Y", "Z"].index(
+                            {"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]
+                        )
+                        - 1
+                    ]
+                    if ["X", "Y", "Z"].index(
+                        {"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]
+                    )
+                    > 0
+                    else ["X", "Y", "Z"][2]
+                )
+            ]
+            if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 0
+            else {"X": 1, "Y": 2, "Z": 3}[
+                {"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]
+            ]
+            if {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]] == 3
+            else {"X": 1, "Y": 2, "Z": 3}[
+                (
+                    ["X", "Y", "Z"][
+                        ["X", "Y", "Z"].index(
+                            {"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]
+                        )
+                        + 1
+                    ]
+                    if ["X", "Y", "Z"].index(
+                        {"A": "X", "B": "Y", "C": "Z"}[line.strip().split()[0]]
+                    )
+                    < 2
+                    else ["X", "Y", "Z"][0]
+                )
+            ]
+            for line in open(fileName, "r")
+        ]
+        + [
+            {"X": 0, "Y": 3, "Z": 6}[line.strip().split()[1]]
+            for line in open(fileName, "r")
+        ]
+    )
+
 
 if __name__ == "__main__":
     import os
-    print(part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"), part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"))
+
+    print(
+        part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+        part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+    )
--- Day 3/day3.py
+++ Day 3/day3.py
@@ -1,12 +1,63 @@
 def part1(fileName):
-    return sum([ord(list(set(line[len(line)//2:]).intersection(set(line[0:len(line)//2])))[0]) - 38 if list(set(line[len(line)//2:]).intersection(set(line[0:len(line)//2])))[0].isupper() else ord(list(set(line[len(line)//2:]).intersection(set(line[0:len(line)//2])))[0]) - 96 for line in open(fileName, "r")])
+    return sum(
+        [
+            ord(
+                list(
+                    set(line[len(line) // 2 :]).intersection(
+                        set(line[0 : len(line) // 2])
+                    )
+                )[0]
+            )
+            - 38
+            if list(
+                set(line[len(line) // 2 :]).intersection(set(line[0 : len(line) // 2]))
+            )[0].isupper()
+            else ord(
+                list(
+                    set(line[len(line) // 2 :]).intersection(
+                        set(line[0 : len(line) // 2])
+                    )
+                )[0]
+            )
+            - 96
+            for line in open(fileName, "r")
+        ]
+    )
+
 
 def part2(fileName):
-    return sum([ord(list(set(list(open(fileName, 'r'))[idx].strip()).intersection(set(list(open(fileName, 'r'))[idx + 1])).intersection(set(list(open(fileName, 'r'))[idx + 2])))[0]) - 38 if list(set(list(open(fileName, 'r'))[idx].strip()).intersection(set(list(open(fileName, 'r'))[idx + 1])).intersection(set(list(open(fileName, 'r'))[idx + 2])))[0].isupper() else ord(list(set(list(open(fileName, 'r'))[idx].strip()).intersection(set(list(open(fileName, 'r'))[idx + 1])).intersection(set(list(open(fileName, 'r'))[idx + 2])))[0]) - 96 for idx in range(0, len(list(open(fileName, 'r'))), 3)])
-    
-        
-        
-    
+    return sum(
+        [
+            ord(
+                list(
+                    set(list(open(fileName, "r"))[idx].strip())
+                    .intersection(set(list(open(fileName, "r"))[idx + 1]))
+                    .intersection(set(list(open(fileName, "r"))[idx + 2]))
+                )[0]
+            )
+            - 38
+            if list(
+                set(list(open(fileName, "r"))[idx].strip())
+                .intersection(set(list(open(fileName, "r"))[idx + 1]))
+                .intersection(set(list(open(fileName, "r"))[idx + 2]))
+            )[0].isupper()
+            else ord(
+                list(
+                    set(list(open(fileName, "r"))[idx].strip())
+                    .intersection(set(list(open(fileName, "r"))[idx + 1]))
+                    .intersection(set(list(open(fileName, "r"))[idx + 2]))
+                )[0]
+            )
+            - 96
+            for idx in range(0, len(list(open(fileName, "r"))), 3)
+        ]
+    )
+
+
 if __name__ == "__main__":
     import os
-    print(part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"), part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"))
+
+    print(
+        part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+        part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+    )

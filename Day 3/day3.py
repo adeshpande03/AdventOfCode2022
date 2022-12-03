43dff02f973f4f29db413f3a8c43e55388a75b32
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-02 23:59:47 -0600

    syntax

--- Day 3/day3.py
+++ Day 3/day3.py
@@ -9,4 +9,4 @@ def part2(fileName):
     
 if __name__ == "__main__":
     import os
-    print(part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"))
+    print(part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"), part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"))

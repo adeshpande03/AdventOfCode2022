ecf7afb064080b0f8c6a666dea77b6df4eb86225
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-04 14:37:01 -0600

    One liner for both parts in O(n) time, black

--- Day 4/day4.py
+++ Day 4/day4.py
@@ -1,38 +1,47 @@
-def part1():
-    f = list(open("input.txt", "r"))
-    il = []
-    for line in f:
-        line = line.strip()
-        if line:
-            line = line.split(",")
-            line0 = line[0]
-            line1 = line[1]
-            line0 = list(range((int(line0.split("-")[0])), (int(line0.split("-")[1]) + 1)))
-            line1 = list(range((int(line1.split("-")[0])), (int(line1.split("-")[1]) + 1)))
-            il.append(line0)
-            il.append(line1)
-    c = 0
-    for i in range(0, len(il), 2):
-        if (len(set(il[i + 1]).intersection(set(il[i]))) < len(il[i+ 1]) or len(set(il[i + 1]).intersection(set(il[i]))) < len(il[i]) ):
-            c += 1
-    print(c)
+def part1(fileName):
+    return sum(
+        [
+            (
+                int(line.split(",")[0].split("-")[0])
+                >= int(line.split(",")[1].split("-")[0])
+                and int(line.split(",")[0].split("-")[1])
+                <= int(line.split(",")[1].split("-")[1])
+            )
+            or (
+                int(line.split(",")[1].split("-")[0])
+                >= int(line.split(",")[0].split("-")[0])
+                and int(line.split(",")[1].split("-")[-1])
+                <= int(line.split(",")[0].split("-")[-1])
+            )
+            for line in list(open(fileName, "r"))
+            if len(line) > 1
+        ]
+    )
 
-def part2():
-    f = list(open("input.txt", "r"))
-    il = []
-    for line in f:
-        line = line.strip()
-        if line:
-            line = line.split(",")
-            line0 = line[0]
-            line1 = line[1]
-            line0 = list(range((int(line0.split("-")[0])), (int(line0.split("-")[1]) + 1)))
-            line1 = list(range((int(line1.split("-")[0])), (int(line1.split("-")[1]) + 1)))
-            il.append(line0)
-            il.append(line1)
-    c = 0
-    for i in range(0, len(il), 2):
-        if (len(set(il[i + 1]).intersection(set(il[i]))) > 0):
-            c += 1
-    print(c)
-part2()
+
+def part2(fileName):
+    return sum(
+        [
+            not (
+                (
+                    int(line.split(",")[0].split("-")[1])
+                    < int(line.split(",")[1].split("-")[0])
+                )
+                or (
+                    int(line.split(",")[1].split("-")[1])
+                    < int(line.split(",")[0].split("-")[0])
+                )
+            )
+            for line in list(open(fileName, "r"))
+            if len(line) > 1
+        ]
+    )
+
+
+if __name__ == "__main__":
+    import os
+
+    print(
+        part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+        part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+    )

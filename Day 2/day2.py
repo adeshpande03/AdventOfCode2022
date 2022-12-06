4499a6c0305b927644772549c83cac8f14914ac0
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-05 23:37:14 -0600

    commented out good code

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -16,6 +16,23 @@ def part1(fileName):
             for line in open(fileName, "r")
         ]
     )
+    
+    # key = {"X": 1, "Y": 2, "Z": 3}
+    # f = open(filename, "r")
+
+    # def matchup(opponent, me):
+    #     opponent = ["A", "B", "C"].index(opponent)
+    #     me = ["X", "Y", "Z"].index(me)
+    #     return 6 if me - opponent in [1, -2] else 3 if me - opponent == 0 else 0
+
+    # return sum(
+    #     [
+    #         key.get(line.strip().split()[1])
+    #         + matchup(line.strip().split()[0], line.strip().split()[1])
+    #         for line in f
+    #     ]
+    # )
+
 
 
 def part2(fileName):
@@ -64,7 +81,41 @@ def part2(fileName):
         ]
     )
 
+    # f = open(filename, "r")
+    # oppPossible = ["X", "Y", "Z"]
+    # resultKey = {"X": 0, "Y": 3, "Z": 6}
+    # choiceKey = {"X": 1, "Y": 2, "Z": 3}
+    # matchingOppToMe = {"A": "X", "B": "Y", "C": "Z"}
 
+    # def pickMe(opponent, result):
+    #     if result == 0:
+    #         myChoice = (
+    #             oppPossible[oppPossible.index(opponent) - 1]
+    #             if oppPossible.index(opponent) > 0
+    #             else oppPossible[2]
+    #         )
+    #     elif result == 3:
+    #         myChoice = opponent
+    #     elif result == 6:
+    #         myChoice = (
+    #             oppPossible[oppPossible.index(opponent) + 1]
+    #             if oppPossible.index(opponent) < 2
+    #             else oppPossible[0]
+    #         )
+    #     return choiceKey[myChoice]
+
+    # return sum(
+    #     [
+    #         pickMe(
+    #             matchingOppToMe[line.strip().split()[0]],
+    #             resultKey[line.strip().split()[1]],
+    #         )
+    #         + resultKey[line.strip().split()[1]]
+    #         for line in f
+    #     ]
+    # )
+    
+    
 if __name__ == "__main__":
     import os
 

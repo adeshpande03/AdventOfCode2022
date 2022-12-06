b7d054a511f185b3c9ea33d4142f746824719403
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-05 23:31:32 -0600

    Day 6 1 liner both parts

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,9 +1,50 @@
 def part1(fileName):
-	f = open(fileName)
+
+    return (
+        next(
+            (
+                index
+                for index, value in enumerate(
+                    [
+                        i if len(set(open(fileName).readline()[i : i + 4])) == 4 else 0
+                        for i in range(len(open(fileName).readline()))
+                    ]
+                )
+                if value != 0
+            ),
+            None,
+        )
+        + 4
+    )
+    f = open(fileName).readline()
+    for i in range(0, len(f)):
+        if len(set(f[i : i + 4])) == 4:
+            return i + 4
+
 
 def part2(fileName):
-	f = open(fileName)
-
+    return (
+        next(
+            (
+                index
+                for index, value in enumerate(
+                    [
+                        i
+                        if len(set(open(fileName).readline()[i : i + 14])) == 14
+                        else 0
+                        for i in range(len(open(fileName).readline()))
+                    ]
+                )
+                if value != 0
+            ),
+            None,
+        )
+        + 14
+    )
+    f = open(fileName).readline()
+    for i in range(0, len(f)):
+        if len(set(f[i : i + 14])) == 14:
+            return i + 14
 
 
 if __name__ == "__main__":
@@ -14,5 +55,5 @@ if __name__ == "__main__":
         part2(f"{os.path.dirname(os.path.realpath(__file__))}/test.txt"),
         part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
         part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
-        sep = "\n"
+        sep="\n",
     )

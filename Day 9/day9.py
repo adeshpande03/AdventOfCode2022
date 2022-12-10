ff62afc726d8e07e10fd83b8a5ed103234b858ed
Akhil Deshpande <68347350+adeshpande03@users.noreply.github.com>
2022-12-09 22:57:15 -0600

    Part 2

--- Day 9/day9.py
+++ Day 9/day9.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def part1(fileName):
     visited = set()
     f = list(open(fileName))
@@ -58,7 +60,39 @@ def part1(fileName):
 
 
 def part2(fileName):
-    return 0
+    f = list(open(fileName))
+    def follow(head, tail):
+        rowMove = head[0] - tail[0]
+        columnMove = head[1] - tail[1]
+        if abs(rowMove) <= 1 and abs(columnMove) <= 1:
+            pass
+        elif abs(rowMove) >= 2 and abs(columnMove) >= 2:
+            tail =((head[0] - 1 if tail[0] < head[0] else head[0] + 1), (head[1] - 1 if tail[1] < head[1] else head[1] + 1))
+        elif abs(columnMove) >= 2:
+            tail = (head[0], head[1] - 1 if tail[1] < head[1] else head[1] + 1)
+        elif abs(rowMove) >= 2:
+            tail = (head[0] - 1 if tail[0] < head[0] else head[0] + 1, head[1])
+        return tail
+    head = 0,0
+    tailList = [(0, 0) for _ in range(9)]
+    print(tailList[8])
+    rowDirection = {'L': 0, 'U': -1, 'R': 0, 'D': 1}
+    columnDirection = {'L': -1, 'U': 0, 'R': 1, 'D': 0}
+    set1 = set()
+    set2 = set()
+    set1.add(tailList[0])
+    set2.add(tailList[8])
+    for location in f:
+        direction, numCount = location.split(' ')
+        numCount = int(numCount)
+        for _ in range(numCount):
+            head = (head[0] + rowDirection[direction], head[1]+columnDirection[direction])
+            tailList[0] = follow(head, tailList[0])
+            for i in range(1, 9):
+                tailList[i] = follow(tailList[i-1], tailList[i])
+            set1.add(tailList[0])
+            set2.add(tailList[8])
+    return len(set2)
 
 
 if __name__ == "__main__":
@@ -66,8 +100,8 @@ if __name__ == "__main__":
 
     print(
         # part1(f"{os.path.dirname(os.path.realpath(__file__))}/test.txt"),
-        part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+        # part1(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
         # part2(f"{os.path.dirname(os.path.realpath(__file__))}/test.txt"),
-        # part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
+        part2(f"{os.path.dirname(os.path.realpath(__file__))}/input.txt"),
         sep="\n",
     )
